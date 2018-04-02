266b86334cb7b1afa698cc43987c79bea38e47b0
Dion Arya Pamungkas <dionarya.p@gmail.com>
2018-04-02 18:29:41 +0700

    upgrade to v1.0.1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 var axios = require('axios')
 module.exports = {
     get_response: function (msg, callback) {
-        axios.get(`${process.env.URL_BOT}${msg}`)
+        axios.get(`https://chatbot-indo.herokuapp.com/get/${msg}`)
         .then(result => callback(null, result))
         .catch(err => callback(err, null))
     }
